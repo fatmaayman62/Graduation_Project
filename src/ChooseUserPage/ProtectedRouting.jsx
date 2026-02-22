0b546bee6b3b543866b8b3d33524039802dc8d1c
fatmaayman62 <fatma.ayman5674@gmail.com>
2026-02-22 19:06:33 +0200

    Update ProtectedRouting with registration path check
    Enhance ProtectedRouting to redirect based on registration path.

--- src/ChooseUserPage/ProtectedRouting.jsx
+++ src/ChooseUserPage/ProtectedRouting.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 function ProtectedRouting({children}) {
     const savedData = JSON.parse(localStorage.getItem("userDataReg"))||null;
+    const location = useLocation();
+    const pathName = "/"+(location.pathname).split("/")[1];
+    console.log(pathName)
 
-    if(savedData&&savedData.login===true){
+    if(savedData&&savedData.login===true&&savedData.reg===pathName){
 
         return children
+    }else if((savedData&&savedData.login===true)&&savedData.reg!==pathName){
+        return <Navigate to={savedData.reg} replace />
+
     }else{
         return <Navigate to={"/"} replace />
     }
